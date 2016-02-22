4fa31a4ab4a7fd07064e97bc2197504d2698f494
LudoBike <ludovicdruette@gmail.com>
2016-02-22 15:58:15 +0100

    Resolve reference problem

--- include/libmath/math.hpp
+++ include/libmath/math.hpp
@@ -3,14 +3,57 @@
 
 
 //templates function
+namespace math
+{
+    template<typename T>
+    std::enable_if_t<std::is_arithmetic<T>{}, T> 
+        abs(T const x);
+
+    template<typename T>
+    std::enable_if_t<std::is_arithmetic<T>{}, T>
+        fmod(T numer, T denom);
+
+    template<typename T>
+    std::enable_if_t<std::is_integral<T>{}, T>
+        pgcd(T const x, T const y);
+
+    template<typename T>
+    std::enable_if_t<std::is_integral<T>{}, T>
+        pow(T const base, T const exp);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        pow(T const base, T const exp);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        sqrt(T const square);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        cbrt(T const cube);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        hypot(T const x, T const y);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        exp(T const x);
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>{}, T>
+        ln(T const x);
+}
+
 #include "abs.hpp"
-#include "exp.hpp"
 #include "fmod.hpp"
+#include "pgcd.hpp"
 #include "pow.hpp"
 #include "sqrt.hpp"
 #include "cbrt.hpp"
 #include "hypot.hpp"
-#include "pgcd.hpp"
+#include "exp.hpp"
 #include "ln.hpp"
 
 
